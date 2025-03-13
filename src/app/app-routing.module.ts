3bb6b67b27c96abde28f8d7e5db96103fca92125
AnabelenScript <ana0507belen@gmail.com>
2025-03-13 00:27:00 -0600

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginUserComponent } from './users/UI/pages/login-user/login-user.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: '', component: LoginUserComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginUserComponent } from './users/UI/pages/login-user/login-user.component';
+import { LoginFormComponent } from './users/UI/components/login-form/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginUserComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
